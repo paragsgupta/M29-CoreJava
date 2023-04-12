072cf53e79fc720e27b3dff70aa2fd36dd8cca9c
paragsgupta <guptaparag300100@gmail.com>
2023-04-12 17:47:32 +0530

    dayfour commit

---
+++ dayfour/src/org/tnsif/firstpackage/AccessSpecifierExecuter.java
@@ -0,0 +1,20 @@
+package org.tnsif.firstpackage;
+
+public class AccessSpecifierExecuter {
+
+	public static void main(String[] args) {
+		 Base b=new Base();
+		 
+		 b.methodDefault();
+		 b.methodPublic();
+		 b.methodProtected();
+		 
+		 /*b.mrthod.Private(); --> this Private member, we can't 
+		 access into another class*/
+		 
+		 
+		 //
+	
+	}
+
+}
---
+++ dayfour/src/org/tnsif/secondpackage/Executor.java
@@ -0,0 +1,20 @@
+package org.tnsif.secondpackage;
+
+// import org.tnsif.firstpackage.*;
+
+import org.tnsif.firstpackage.Base;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Base b1=new Base();
+		
+		
+		
+		b1.methodPublic();
+
+	}
+
+}
