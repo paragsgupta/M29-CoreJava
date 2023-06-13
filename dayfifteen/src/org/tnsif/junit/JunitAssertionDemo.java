28e961a379e304120d864c953f39bf8a77ffbf96
paragsgupta <guptaparag300100@gmail.com>
2023-06-13 18:28:23 +0530

    first commit

---
+++ dayfifteen/src/org/tnsif/junit/JunitAssertionDemo.java
@@ -0,0 +1,14 @@
+package org.tnsif.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JunitAssertionDemo {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
